ec041a207dcf2bd6bc14a3b44f53122822a5d7b0
emsabbatini8416 <emmanuel.sabbatini@gmail.com>
2024-02-12 11:43:01 -0500

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+const COUNTRIES: { id: number, label: string }[] = [
+  { id: 1, label: 'USA' },
+  { id: 2, label: 'India' },
+  { id: 3, label: 'France' },
+]
+
+const INITIAL_STATE: { [key: string]: boolean; } = COUNTRIES.reduce((o, key) => ({ ...o, [key.label]: false}), {})
+
 function App() {
+  const [checkedAll, setCheckedAll] = useState(false);
+  const [checked, setChecked] = useState(INITIAL_STATE);
+
+  const selectAll = (value: boolean) => {
+    setCheckedAll(prevState => !prevState)
+    setChecked(prevState => {
+      const newState = { ...prevState };
+      for (const key in newState) {
+        newState[key] = value;
+      }
+      return newState;
+    });
+  }
+
+  const toggleCheck = (key: string) => {
+    const prevValue = checked[key]
+    setChecked(prevState => ({ ...prevState, [key]: !prevValue }))
+    if (prevValue && checkedAll) setCheckedAll(false)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div>
+        <label>Select All</label>
+        <input
+          type="checkbox"
+          onChange={(event) => selectAll(event.target.checked)}
+          checked={checkedAll}
+        />
+      </div>
+      {COUNTRIES.map(country => (
+        <div>
+          <label>{country.label}</label>
+          <input
+            type="checkbox"
+            name={country.label}
+            onChange={() => toggleCheck(country.label)}
+            checked={checked[country.label]}
+            />
+        </div>
+      ))}
     </div>
   );
 }
